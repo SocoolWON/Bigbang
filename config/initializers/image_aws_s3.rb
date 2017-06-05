1664557bdfb81ab9d61f2e6aae10633ce2959659
Socool <hi.seokhowon@gmail.com>
2017-06-06 01:19:53 +0900

    ~ing

--- config/initializers/image_aws_s3.rb
+++ config/initializers/image_aws_s3.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJHRPVGFWI2Q4Y5EQ',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     '',                        # required
+    aws_secret_access_key: '',                        # required
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
     endpoint:              'http://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'teacher-tinytalk'                          # required
+  config.fog_directory  = ''                          # required
 end
 
